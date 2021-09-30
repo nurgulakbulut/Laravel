4349f969a3ec11ddeb5fb84b5091556973821d7d
nurgulakbulut <nurgulakbulut.95@gmail.com>
2021-09-30 19:46:32 +0000

    Add example

---
+++ resources/views/articles/create.blade.php
@@ -0,0 +1,17 @@
+<form action="/articles" method="POST">
+    @csrf
+
+    Baslik<br>
+    <input type="text" name="title" value="{{ old('title') }}">
+    @error('title')
+         <br>{{ $message }}
+    @enderror<hr>
+    Icerik<br>
+    <textarea name="content"> {{ old('content') }} </textarea>
+    @error('content')
+          <br>{{ $message }}
+    @enderror<br>
+    
+    <button type="submit"> Ekle </button>  
+</form>
+<a href="/articles"> Iptal </a>
---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ArticleController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('articles',[ArticleController::class, 'index']);
+
+Route::get('articles/new',[ArticleController::class, 'create']);
+
+Route::get('articles/{id}',[ArticleController::class, 'show']);
+
+Route::post('articles', [ArticleController::class, 'store']);
